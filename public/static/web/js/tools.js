0583d89ae6d24ca6de23a9e2261e9352e11e8cbd
bilulanlv <bilulanlv168@gmail.com>
2022-01-10 16:47:43 +0800

    update

--- public/static/web/js/tools.js
+++ public/static/web/js/tools.js
@@ -1,63 +1,433 @@
-function init(){localStorage.getItem("brsPrivacy")||document.getElementById("privacy_alert").classList.remove("d-none");let e=[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));e.map(function(e){return new bootstrap.Tooltip(e)});let o=window.location.pathname.substring(1);if(console.log(o),1<o.length)try{document.getElementById(o).classList.add("active");let e=document.getElementById("countries");(-1!=o.indexOf("-phone-number")&&"upcoming-phone-number"!==o||"country"===o)&&e.classList.add("active")}catch(e){let t=/receive-sms-from-([a-zA-Z]+)/;var n=t.exec(o);document.getElementById(n[1]+"-phone-number").classList.add("active"),document.getElementById("countries").classList.add("active")}}function stretchBottom(){var e=screen.availHeight;let t=document.getElementById("foot-null");e=e-t.offsetTop-232;0<e&&(t.style.height=e+"px")}function unload(){window.addEventListener("unload",function(e){console.log(1)})}function clickLoading(){let e=document.getElementsByClassName("click_loading");for(var t in e)e[t].onclick=function(){loading(!0)}}function scrollTop(){window.onscroll=function(e){let t=document.getElementById("top");document.documentElement.scrollTop>.1*document.body.clientHeight?t.classList.remove("d-none"):t.classList.add("d-none")}}function toast(e,t={}){void 0===t.type&&(t.type="success"),void 0===t.title&&(t.title="MESSAGE"),void 0===t.isClose&&(t.isClose=!0),void 0===t.showTime&&(t.showTime=5e3);let o=Math.floor(99999*Math.random());e='<div id="toast'+o+'" class="toast bg-'+t.type+' text-light m-1" aria-atomic="true" data-bs-autohide="'+t.isClose+'" data-bs-delay="'+t.showTime+`" style="width:450px;">
-						  <div class="toast-header bg-`+t.type+` text-light">
-						    <strong class="me-auto">`+t.title+`</strong>
+window.onload = function () {
+	init();
+	scrollTop();
+	clickLoading();
+	unload();
+	stretchBottom();
+	if (typeof copyPhone === 'function'){
+		copyPhone();
+	}
+	if (typeof messageInit === 'function'){
+		messageInit();
+	}
+}
+
+function init() {
+	//privacy alert show
+	if (!localStorage.getItem('brsPrivacy')){
+		document.getElementById('privacy_alert').classList.remove('d-none');
+	}
+	//tooltip
+	let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+	let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl)
+	})
+
+	//nav active
+	let pathname = window.location.pathname.substring(1);
+	if (pathname.length > 1){
+		try {
+			document.getElementById(pathname).classList.add('active');
+			let countries = document.getElementById('countries');
+			if (pathname.indexOf('-phone-number') != -1 && pathname !== 'upcoming-phone-number' || pathname === 'country'){
+				countries.classList.add('active');
+			}
+		}catch (e) {
+			try {
+				let regExp = /receive-sms-from-([a-zA-Z]+)/;
+				let link_title = regExp.exec(pathname);
+				document.getElementById(link_title[1] + '-phone-number').classList.add('active');
+				document.getElementById('countries').classList.add('active');
+			}catch (e) {
+				
+			}
+		}
+	}
+}
+
+//底部拉伸
+function stretchBottom(){
+	let windows_height = screen.availHeight;
+	let foot_null = document.getElementById('foot-null');
+	let full_height = foot_null.offsetTop;
+	let height = windows_height - full_height - 232;
+	if (height > 0){
+		foot_null.style.height = height + 'px';
+	}
+}
+
+function unload(){
+	window.addEventListener("unload", function(e) {
+		//虽然无用，但是这里不能删，删除后，loading()后再返回页面，loading不会关闭
+		console.log(1)
+	});
+}
+
+function clickLoading(){
+	let list = document.getElementsByClassName('click_loading'); // 获取class为ant-btn的元素
+	for(let i in list)
+	{
+		list[i].onclick=function() {
+			loading(true);
+		};
+	}
+}
+
+//top button
+function scrollTop() {
+	window.onscroll = function (e) {
+		let top = document.getElementById('top');
+		if (document.documentElement.scrollTop > (document.body.clientHeight*0.1)) {
+			top.classList.remove('d-none')
+		} else {
+			top.classList.add('d-none')
+		}
+	}
+}
+
+//toast
+function toast(message, params = {}) {
+	if (params.type === undefined) {
+		params.type = 'success';
+	}
+	if (params.title === undefined) {
+		params.title = 'MESSAGE';
+	}
+	//auto close
+	if (params.isClose === undefined) {
+		params.isClose = true;
+	}
+	if (params.showTime === undefined) {
+		params.showTime = 5000;
+	}
+
+	let random = Math.floor(Math.random() * 99999);
+	let messageHtml = `<div id="toast` + random + `" class="toast bg-` + params.type +
+		` text-light m-1" aria-atomic="true" data-bs-autohide="` + params.isClose +
+		`" data-bs-delay="` + params.showTime + `" style="width:450px;">
+						  <div class="toast-header bg-` + params.type + ` text-light">
+						    <strong class="me-auto">` + params.title + `</strong>
 						    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close"></button>
 						  </div>
 						  <div class="toast-body">
-						    `+e+`
+						    ` + message + `
 						  </div>
-						</div>`;let n=document.getElementById("toast-box");n.innerHTML+=e;let a=[].slice.call(document.querySelectorAll(".toast")),d=a.map(function(e){return new bootstrap.Toast(e,{animation:!0})});d.forEach(e=>e.show());let i=document.getElementById("toast"+o);i.addEventListener("hidden.bs.toast",function(e){let t=document.getElementById("toast"+o);t.parentNode.removeChild(t)})}function modal(e,t={},o,n){void 0===t.title&&(t.title="Message"),void 0===t.closeButton&&(t.closeButton="Close"),void 0===t.confirmButton&&(t.confirmButton="Confirm"),void 0===t.confirmButton&&(t.confirmButton="Confirm"),void 0===t.size&&(t.size="modal-lg"),void 0===t.autoClose&&(t.autoClose=!0);e=`<div class="modal fade" id="modal" data-bs-backdrop="false" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-						<div class="modal-dialog modal-dialog-scrollable `+t.size+` modal-dialog-centered">
+						</div>`;
+	let toast_box = document.getElementById('toast-box');
+	toast_box.innerHTML += messageHtml;
+	let toastElList = [].slice.call(document.querySelectorAll('.toast'))
+	let toastList = toastElList.map(function (toastEl) {
+		return new bootstrap.Toast(toastEl, {
+			animation: true
+		})
+	})
+	toastList.forEach(toast => toast.show());
+
+	let myToastEl = document.getElementById('toast' + random)
+	myToastEl.addEventListener('hidden.bs.toast', function (e) {
+		let toastC = document.getElementById("toast" + random)
+		toastC.parentNode.removeChild(toastC)
+	})
+}
+
+//modal
+function modal(body, params = {}, confirmCallback, closeCallback) {
+	if (params.title === undefined) {
+		params.title = 'Message';
+	}
+	if (params.closeButton === undefined) {
+		params.closeButton = 'Close';
+	}
+	if (params.confirmButton === undefined) {
+		params.confirmButton = 'Confirm';
+	}
+	if (params.confirmButton === undefined) {
+		params.confirmButton = 'Confirm';
+	}
+	if (params.size === undefined) {
+		params.size = 'modal-lg';
+	}
+	if (params.autoClose === undefined) {
+		params.autoClose = true;
+	}
+
+	let modalHtml = `<div class="modal fade" id="modal" data-bs-backdrop="false" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+						<div class="modal-dialog modal-dialog-scrollable ` + params.size + ` modal-dialog-centered">
 							<div class="modal-content">
 								<div class="modal-header">
-									<h5 class="modal-title" id="modal-title">`+t.title+`</h5>
+									<h5 class="modal-title" id="modal-title">` + params.title + `</h5>
 									<button type="button" class="btn-close" aria-label="Close" id="icon_close"></button>
 								</div>
 								<div class="modal-body" id="modal-body">
-									`+e+`
+									` + body + `
 								</div>
 								<div class="modal-footer">
-									<button type="button" class="btn btn-secondary" id="close">`+t.closeButton+`</button>
+									<button type="button" class="btn btn-secondary" id="close">` + params.closeButton + `</button>
 									<button type="button" class="btn btn-primary" id="confirm">
 									<span class="spinner-border spinner-border-sm d-none" role="status" aria-hidden="true" id="confirm_loading"></span>
-                                    `+t.confirmButton+`
+                                    ` + params.confirmButton + `
                                     </button>
 								</div>
 							</div>
 						</div>
-					</div>`;document.body.insertAdjacentHTML("beforeend",e);let a=document.getElementById("modal"),d=new bootstrap.Modal(a,{backdrop:"static"});d.toggle();let i=document.getElementById("confirm"),l=document.getElementById("close"),s=document.getElementById("icon_close");i.addEventListener("click",function(e){t.autoClose&&(d.hide(),a.parentNode.removeChild(a)),o&&o(d)}),l.addEventListener("click",function(e){d.hide(),n&&n(d),a.parentNode.removeChild(a)}),s.addEventListener("click",function(e){d.hide(),a.parentNode.removeChild(a)})}function modalClose(e,t){e.hide();let o=document.getElementById("modal");e=o.parentNode.removeChild(o);t&&t(e),loading(!1)}function loading(e=!0,t=0,o=!0){let n=document.getElementById("loading"),a=document.getElementById("shade");e?(n.classList.remove("d-none"),o&&a.classList.remove("d-none"),0<t&&setTimeout(function(){n.classList.add("d-none"),a.classList.add("d-none")},1e3*t)):(n.classList.add("d-none"),a.classList.add("d-none"))}function copy(){let e=new ClipboardJS(".copy");toast("Copy success!!!"),e.on("error",function(e){toast("Copy fail!!!",{type:"danger"})})}function referrer(){let e="";0<document.referrer.length&&(e=document.referrer);try{0==e.length&&0<opener.location.href.length&&(e=opener.location.href)}catch(e){}return e}function captchaCallback(e){e&&$.ajax({url:"/hcaptcha",type:"post",data:{response:e},beforeSend:function(){loading()},success:function(e){console.log(e),0==e.error_code?(toast(e.msg),window.location.href=referrer()):toast(e.msg,{type:"danger"})},complete:function(){loading(!1)}})}function subscription(e){modal(`
+					</div>`;
+	document.body.insertAdjacentHTML('beforeend', modalHtml);
+	let modalEl = document.getElementById('modal');
+	let myModal = new bootstrap.Modal(modalEl, {backdrop: 'static'})
+	myModal.toggle()
+
+	let confirm = document.getElementById('confirm');
+	let close = document.getElementById('close');
+	let iconClose = document.getElementById('icon_close');
+	////press the Confirm button to trigger
+	confirm.addEventListener('click', function (e) {
+		if (params.autoClose) {
+			myModal.hide();
+			modalEl.parentNode.removeChild(modalEl)
+		}
+		if (confirmCallback) {
+			confirmCallback(myModal);
+		}
+
+	})
+	//press the off button to trigger
+	close.addEventListener('click', function (e) {
+		myModal.hide()
+		if (closeCallback) {
+			closeCallback(myModal);
+		}
+		modalEl.parentNode.removeChild(modalEl)
+	})
+
+	//press the Close button to trigger
+	iconClose.addEventListener('click', function (e) {
+		myModal.hide()
+		modalEl.parentNode.removeChild(modalEl)
+	})
+}
+
+//close mosal
+function modalClose(modal, callback) {
+	modal.hide();
+	let modalEl = document.getElementById('modal');
+	let rmodal = modalEl.parentNode.removeChild(modalEl);
+
+	if (callback){
+		callback(rmodal);
+	}
+	loading(false);
+}
+
+//loading
+function loading(type = true,autoClose = 0, shadeType = true) {
+	let loading = document.getElementById('loading');
+	let shade = document.getElementById('shade');
+	if (type) {
+		loading.classList.remove("d-none");
+		if (shadeType) {
+			shade.classList.remove("d-none");
+		}
+		if (autoClose > 0){
+			setTimeout(function () {
+				loading.classList.add("d-none");
+				shade.classList.add("d-none");
+			},autoClose*1000)
+		}
+	} else {
+		loading.classList.add("d-none");
+		shade.classList.add("d-none");
+	}
+}
+
+//copy
+function copy() {
+	let clipboard = new ClipboardJS('.copy')
+	toast('Copy success!!!')
+	clipboard.on('error', function (e) {
+		toast('Copy fail!!!', {type: 'danger'})
+	})
+}
+
+function referrer() {
+	let ref = '';
+	if (document.referrer.length > 0) {
+		ref = document.referrer;
+	}
+	try {  if (ref.length == 0 && opener.location.href.length > 0) {
+		ref = opener.location.href;
+	}
+	} catch (e) {}
+	return ref;
+}
+
+function captchaCallback (e) {
+	if (e){
+		$.ajax({
+			url: '/hcaptcha',
+			type: 'post',
+			data: {response: e},
+			beforeSend: function(){
+				loading();
+			},
+			success: function (e) {
+				console.log(e)
+				if (e.error_code == 0) {
+					toast(e.msg);
+					window.location.href = referrer();
+				} else {
+					toast(e.msg, {type: 'danger'})
+				}
+			},
+			complete: function () {
+				loading(false);
+			}
+		})
+	}
+}
+
+function subscription(lang) {
+	let subscriptionHtml = `
 		<div class="container">
 			<div class="row">
 				<div class="modal-body">
 				<form id="subscription_form">
 					<div class="form-floating mb-3">
-					  <input class="form-control" placeholder="`+e.emailAddress+`" id="email_address" required onfocus="inputFocus('subscription_form')">
-					  <label for="email_address" class="text-secondary">`+e.emailAddress+`</label>
+					  <input class="form-control" placeholder="`+lang.emailAddress+`" id="email_address" required onfocus="inputFocus('subscription_form')">
+					  <label for="email_address" class="text-secondary">`+lang.emailAddress+`</label>
 				  	</div>
 				  <div class="alert alert-secondary" role="alert">
-					  `+e.explain+`
+					  ` + lang.explain + `
 				  </div>
 				</form>
 			  </div>
 			</div>
 		</div>
-	`,{title:e.title,size:"",autoClose:!1},function(e){inputFocus("subscription_form");var t=e;loadScript(function(){var e=$("#email_address").val();$.ajax({url:"/api/mailbox",type:"post",data:{mailbox:e},beforeSend:function(){$("#confirm").attr("disabled","disabled"),$("#confirm_loading").removeClass("d-none")},success:function(e){0==e.error_code?(toast(e.msg),modalClose(t)):toast(e.msg,{type:"danger"})},complete:function(){$("#confirm").removeAttr("disabled"),$("#confirm_loading").addClass("d-none")}})})},function(e){})}function feedback(e){modal(`
+	`;
+	modal(subscriptionHtml, {title: lang.title, size: '', autoClose: false}, function (e) {
+		inputFocus('subscription_form');
+		var modal = e;
+		loadScript(function () {
+			let email = $('#email_address').val()
+			$.ajax({
+				url: '/api/mailbox',
+				type: 'post',
+				data: {mailbox: email},
+				beforeSend: function(){
+					$("#confirm").attr('disabled', "disabled")
+					$("#confirm_loading").removeClass('d-none')
+				},
+				success: function (e) {
+					if (e.error_code == 0) {
+						toast(e.msg);
+						modalClose(modal)
+					} else {
+						toast(e.msg, {type: 'danger'});
+					}
+				},
+				complete: function () {
+					$('#confirm').removeAttr('disabled')
+					$("#confirm_loading").addClass('d-none')
+				}
+			})
+		});
+	}, function (e) {
+
+	});
+}
+
+function feedback(lang) {
+	let feedbackHtml = `
 		<div class="container">
 			<div class="row">
 				<div class="modal-body">
 				<form id="form_feedback">
 					<div class="form-floating mb-3">
-					  <textarea class="form-control" id="feedback_content" placeholder="`+e.contentPla+`" style="height: 100px" required onfocus="inputFocus('form_feedback')"></textarea>
-                      <label for="floatingTextarea2" class="text-secondary">`+e.contentPla+`</label>
-                      <div class="invalid-feedback">`+e.contentLabel+`</div>
+					  <textarea class="form-control" id="feedback_content" placeholder="`+lang.contentPla+`" style="height: 100px" required onfocus="inputFocus('form_feedback')"></textarea>
+                      <label for="floatingTextarea2" class="text-secondary">`+lang.contentPla+`</label>
+                      <div class="invalid-feedback">`+lang.contentLabel+`</div>
 				  	</div>
 				  	<div class="form-floating mb-3">           
-					  <input class="form-control" id="feedback_email" placeholder="`+e.emailAddress+`" required onfocus="inputFocus('form_feedback')">
-					  <label for="email_address" class="text-secondary">`+e.emailAddress+`</label>
-					  <div class="invalid-feedback">`+e.emailLabel+`</div>
+					  <input class="form-control" id="feedback_email" placeholder="`+lang.emailAddress+`" required onfocus="inputFocus('form_feedback')">
+					  <label for="email_address" class="text-secondary">`+lang.emailAddress+`</label>
+					  <div class="invalid-feedback">`+lang.emailLabel+`</div>
 				  	</div>
 				</form>
 			  </div>
 			</div>
 		</div>
-	`,{title:e.title,size:"",autoClose:!1},function(e){inputFocus("form_feedback");let t=e;loadScript(function(){$("#email_address").val();$.ajax({url:"/api/feedback",type:"post",data:{content:$("#feedback_content").val(),email:$("#feedback_email").val(),type:1},beforeSend:function(){$("#confirm").attr("disabled","disabled"),$("#confirm_loading").removeClass("d-none")},success:function(e){0==e.error_code?(toast(e.msg),modalClose(t)):toast(e.msg,{type:"danger"})},complete:function(){$("#confirm").removeAttr("disabled"),$("#confirm_loading").addClass("d-none")}})})},function(e){})}function buttonLoading(e,t=!0){let o=document.getElementById(e);!0===t?o.classList.remove("d-none"):o.classList.add("d-none")}function loadScript(e,t){var o={jquery:"//"+window.location.host+"/static/web/js/jquery-3.6.0.min.js",copy:"//"+window.location.host+"/static/web/js/clipboard.min.js"};t=void 0===t?o.jquery:o[t];let n=document.createElement("script");n.type="text/javascript",n.readyState?n.onreadystatechange=function(){"loaded"!=n.readyState&&"complete"!=n.readyState||(n.onreadystatechange=null,e())}:n.onload=function(){e()},n.src=t,document.getElementsByTagName("head")[0].appendChild(n)}function inputFocus(e="form_data"){let t=document.getElementById(e);t.classList.add("was-validated")}window.onload=function(){init(),scrollTop(),clickLoading(),unload(),stretchBottom(),"function"==typeof copyPhone&&copyPhone(),"function"==typeof messageInit&&messageInit()};
+	`;
+	modal(feedbackHtml, {title: lang.title, size: '', autoClose: false}, function (e) {
+		inputFocus('form_feedback');
+		let modal = e;
+		loadScript(function () {
+			let email = $('#email_address').val()
+			$.ajax({
+				url: '/api/feedback',
+				type: 'post',
+				data: {content: $("#feedback_content").val(), email: $("#feedback_email").val(), type: 1},
+				beforeSend: function(){
+					$("#confirm").attr('disabled', "disabled")
+					$("#confirm_loading").removeClass('d-none')
+				},
+				success: function (e) {
+					if (e.error_code == 0) {
+						toast(e.msg);
+						modalClose(modal)
+					} else {
+						toast(e.msg, {type: 'danger'});
+					}
+				},
+				complete: function () {
+					$('#confirm').removeAttr('disabled')
+					$("#confirm_loading").addClass('d-none')
+				}
+			})
+		});
+	}, function (e) {
+
+	});
+}
+
+function buttonLoading(buttonID, type = true){
+	let button = document.getElementById(buttonID);
+	if (type === true){
+		button.classList.remove("d-none");
+	}else {
+		button.classList.add("d-none");
+	}
+}
+
+//dynamic loading JS
+function loadScript(callback, url) {
+	let urls = {
+		jquery: '//'+ window.location.host + '/static/web/js/jquery-3.6.0.min.js',
+		copy: '//' + window.location.host + '/static/web/js/clipboard.min.js'
+	};
+	if (url === undefined){
+		url = urls.jquery;
+	}else {
+		url = urls[url];
+	}
+	let script = document.createElement("script")
+	script.type = "text/javascript";
+	if (script.readyState) { //IE
+		script.onreadystatechange = function () {
+			if (script.readyState == "loaded" || script.readyState == "complete") {
+				script.onreadystatechange = null;
+				callback();
+			}
+		};
+	} else { //Others
+		script.onload = function () {
+			callback();
+		};
+	}
+	script.src = url;
+	document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+//input 激活事件
+function inputFocus(id = 'form_data'){
+	let form = document.getElementById(id);
+	form.classList.add('was-validated');
+}
