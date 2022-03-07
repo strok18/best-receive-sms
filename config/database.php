0728517debd962b330ec8c0d111b923612e53c70
yunsimple <yunyun20520@gmail.com>
2022-03-07 20:10:30 +0800

    update

--- config/database.php
+++ config/database.php
@@ -84,7 +84,7 @@ return [
     'db_history' => [
         'type'            => 'mysql',
         // 服务器地址
-        'hostname'        => '23.239.2.123',
+        'hostname'        => '127.0.0.1',
         // 数据库名
         'database'        => 'history',
         // 用户名
